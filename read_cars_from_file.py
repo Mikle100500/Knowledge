e8cd2e99612c7e22358ba2cc6e2795401d639b39
Mikle100500 <mikhail.pavlenko84@gmail.com>
2016-08-10 15:48:13 +0300

    a few method (cars) have been added

---
+++ read_cars_from_file.py
@@ -0,0 +1,12 @@
+from class_cars import Car
+
+save_cars_to_file = []
+
+for car_line in open('input_cars.txt'):
+    data = car_line.split(';')
+    vendor, name = data
+    saved_car = Car(vendor, name)
+    save_cars_to_file.append(saved_car)
+
+output = open('output_cars.txt', 'w')
+output.write(str(save_cars_to_file))
