2ae12a3396dc229c532f00f7b8ebdc8a7877d5c4
Mikle100500 <mikhail.pavlenko84@gmail.com>
2016-08-10 15:51:27 +0300

    read_cars_from_file has been added

--- read_cars_from_file.py
+++ read_cars_from_file.py
@@ -1,12 +1,13 @@
 from class_cars import Car
 
-save_cars_to_file = []
+def save_cars_to_file():
+    save_cars_to_file = []
 
-for car_line in open('input_cars.txt'):
-    data = car_line.split(';')
-    vendor, name = data
-    saved_car = Car(vendor, name)
-    save_cars_to_file.append(saved_car)
+    for car_line in open('input_cars.txt'):
+        data = car_line.split(';')
+        vendor, name = data
+        saved_car = Car(vendor, name)
+        save_cars_to_file.append(saved_car)
 
-output = open('output_cars.txt', 'w')
-output.write(str(save_cars_to_file))
+    output = open('output_cars.txt', 'w')
+    output.write(str(save_cars_to_file))
