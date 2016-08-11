4dc77c75fc9927484c0bc37482e5edc922c5c5aa
Mikle100500 <mikhail.pavlenko84@gmail.com>
2016-08-11 12:27:29 +0300

    weird json

---
+++ json/parse_json.py
@@ -0,0 +1,11 @@
+import json
+
+a = json.loads('{"a": "b"}')
+
+# python_object = {'some_text': 'my text',
+#                  'some_number': 12345,
+#                  'null_value': None,
+#                  'some_list': [1, 2, 3]}
+#
+# json_string = json.dump(python_object)
+# new_python_object = json.load(json_string)
