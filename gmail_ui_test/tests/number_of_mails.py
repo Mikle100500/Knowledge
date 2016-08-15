40ae2cf3e20e8b62e3d2a0dcd698e0d32f867c0d
Mikle100500 <mikhail.pavlenko84@gmail.com>
2016-08-15 18:30:04 +0300

    test letters is done (1 TC)

--- gmail_ui_test/tests/number_of_mails.py
+++ gmail_ui_test/tests/number_of_mails.py
@@ -5,18 +5,17 @@ from gmail_ui_test.wrappers.gmail_wrapper import GmailPage
 
 
 class TestNumberOfMails(unittest.TestCase):
+    # def setUp(self):
+    #     self.driver = webdriver.Firefox()
 
-    def setUpClass(self):
-        self.driver = webdriver.Firefox()
-
-    @staticmethod
-    def page_init():
-        return GmailPage
+    # @staticmethod
+    # def page_init():
+    #     return GmailPage
 
     def test_number_of_mails(self):
-        gmail = self.page_init()
+        gmail = GmailPage()
         gmail.navigate_to_gmail()
-        gmail.login(self,'forTestingJohnTester@gmail.com', 'gmailtest')
+        gmail.login()
         self.assertEquals(gmail.number_of_mails(), 3)
 
 if __name__ == '__main__':
--- gmail_ui_test/wrappers/gmail_wrapper.py
+++ gmail_ui_test/wrappers/gmail_wrapper.py
@@ -6,21 +6,22 @@ class GmailPage:
     def __init__(self):
         self.driver = webdriver.Firefox()
 
-    def navigate_to_gmail(self, driver):
-        driver.get('https://www.google.com/ncr')
-        driver.find_element_by_id('gb_70').click()
+    def navigate_to_gmail(self):
+        self.driver.get('https://www.google.com/ncr')
+        self.driver.find_element_by_id('gb_70').click()
 
-    def login(self, driver, account, password):
-        driver.find_element_by_id('Email').send_keys(account, Keys.ENTER)
-        driver.find_element_by_id('next').click()
+    def login(self):
+        self.driver.find_element_by_id('Email').send_keys('forTestingJohnTester@gmail.com', Keys.ENTER)
+        self.driver.find_element_by_id('next').click()
 
-        driver.find_element_by_id('Passwd').send_keys(password)
-        driver.find_element_by_id('PersistentCookie').click()
-        driver.find_element_by_id('signIn').click()
+        self.driver.find_element_by_id('Passwd').send_keys('gmailtest')
+        self.driver.find_element_by_id('PersistentCookie').click()
+        self.driver.find_element_by_id('signIn').click()
+        self.driver.find_element_by_xpath('.//*[@id="gbw"]/div/div/div[1]/div[2]/a').click()
 
-    def logout(self, driver):
-        driver.find_element_by_id('.//*[@id="gb"]//span').click()
-        driver.find_element_by_id('.//*[@id="gb_71"]').click()
+    def logout(self):
+        self.driver.find_element_by_id('.//*[@id="gb"]//span').click()
+        self.driver.find_element_by_id('.//*[@id="gb_71"]').click()
 
-    def number_of_mails(self, driver):
-        return len(driver.find_elements_by_xpath('//table[contains(@id, ":3")]//tr'))
+    def number_of_mails(self):
+        return len(self.driver.find_elements_by_xpath('//table[contains(@id, ":3")]//tr'))
