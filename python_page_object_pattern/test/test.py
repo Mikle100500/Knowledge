76943c967ef83f710c6e30f4751dec80f655567e
Mikle100500 <mikhail.pavlenko84@gmail.com>
2016-08-17 17:37:34 +0300

    gmail page object - split the class (+BaseTestPage) + assertions

--- python_page_object_pattern/test/test.py
+++ python_page_object_pattern/test/test.py
@@ -3,6 +3,7 @@ from python_page_object_pattern.test.BaseTestPage import BaseTestPage
 
 
 class GoogleSearchTest(BaseTestPage):
+
     def test_01_get_gmail_page(self):
         main_page = pages.MainPage(self.driver)
         assert main_page.is_title_matches()
@@ -13,11 +14,12 @@ class GoogleSearchTest(BaseTestPage):
         login_page.set_email()
         login_page.set_password()
         login_page.load_mail_page()
+        assert self.driver.title == "Входящие - fortestingjohntester@gmail.com - Gmail"
 
     def test_03_count_letters(self):
         email_page = pages.EmailPage(self.driver)
         assert email_page.count_letters_at_the_page() == 3
 
-    def test_04_send_mail(self):
-        mail = pages.EmailPage(self.driver)
-        mail.send_a_letter_to_yourself()
+    # def test_04_send_mail(self):
+    #     mail = pages.EmailPage(self.driver)
+    #     mail.send_a_letter_to_yourself()
